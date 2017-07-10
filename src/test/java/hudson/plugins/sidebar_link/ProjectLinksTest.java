4a52a76f828a3d516ed15597acfe55efe51edc69
Alex Johnson <alexjo5000@gmail.com>
2017-07-10 10:42:44 -0400

    Proof that WorkflowJob doesn't treat JobProps the same as FreeStyle
    * Updated plugins and core to bleeding edge
    * Made Unit Test for proof

--- src/test/java/hudson/plugins/sidebar_link/ProjectLinksTest.java
+++ src/test/java/hudson/plugins/sidebar_link/ProjectLinksTest.java
@@ -6,7 +6,6 @@ package hudson.plugins.sidebar_link;
 
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import hudson.model.AbstractProject;
 import hudson.model.FreeStyleProject;
 import hudson.model.Job;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
@@ -14,6 +13,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -50,5 +52,16 @@ public class ProjectLinksTest {
         rule.submit(form);
         assertNotNull("Method geJobActions should not return null.", links.getJobActions(project));
     }
-    
+
+    @Test
+    public void testLinkActionIsAddedWithJobProps () throws Exception{
+        Job p1 = rule.jenkins.createProject(FreeStyleProject.class, "burt");
+        p1.addProperty(new ProjectLinks(Collections.singletonList(new LinkAction("http://google.com","stop","edit-delete.png"))));
+        assertEquals(1, p1.getActions(LinkAction.class).size());
+
+        Job p2 = rule.jenkins.createProject(WorkflowJob.class, "ernie");
+        p2.addProperty(new ProjectLinks(Collections.singletonList(new LinkAction("http://google.com","stop","edit-delete.png"))));
+        assertEquals(1, p2.getActions(LinkAction.class).size());
+    }
+
 }
